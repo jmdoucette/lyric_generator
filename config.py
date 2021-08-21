fb8e09f6706559da86f799044acbea6548b9f806
jmdoucette <jmdoucette41@gmail.com>
2021-08-21 15:16:26 -0400

    gitignore and config

---
+++ config.py
@@ -0,0 +1,14 @@
+#cleaning
+necessary_count = 10
+
+#training
+batch_size = 16
+buffer_size = 1000
+seq_len = 10
+epochs = 20
+
+#generation
+num_generate = 200
+temp = 1.0
+saved_models_path = "saved_models/"
+current_model = "blues_rock"
